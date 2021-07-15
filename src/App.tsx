0a98ca8da7ce52b1b630865c6df40623e3e09407
toaney <toaney@gmail.com>
2021-07-15 13:07:30 -0700

    add example TypeScript component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Counter from './components/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       React TypeScript Practice
+      <Counter title="TypeScript" subtitle="Typescript with types"/>
     </div>
   );
 }
